aad5944cb6231628fb41f3c78b678d8944077cb7
Ciaran Fisher <cfisher88@gmail.com>
2023-04-14 22:24:41 +0200

    Add mobile stub

---
+++ Mobile/Platforms/Android/MainApplication.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.Runtime;
+using Java.Lang;
+
+namespace Mobile
+{
+    [Application]
+    public class MainApplication : MauiApplication
+    {
+        public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+            : base(handle, ownership)
+        {
+            try
+            {
+                //MUST BE LOADED IN MAIN THREAD
+                JavaSystem.LoadLibrary("opus");
+                JavaSystem.LoadLibrary("speexdsp");
+            }
+            catch (UnsatisfiedLinkError ex)
+            {
+                Console.Error.WriteLine(ex);
+            }
+        }
+
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
